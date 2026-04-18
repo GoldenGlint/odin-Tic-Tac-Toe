cdd77456668f6c3cde63fd89a2d8a892eb539d8a
GoldenGlint <darhgo@gmail.com>
2026-04-18 15:33:43 -0400

    First Draft Console

--- script.js
+++ script.js
@@ -12,7 +12,11 @@ function createPlayer(name, id){
     const checkWin = () => {
         const winningCombos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
         const posSet = new Set(positions);
-        return winningCombos.some(combo => combo.every(i => posSet.has(i)));
+        if(winningCombos.some(combo => combo.every(i => posSet.has(i)))){
+            console.log(console.log(`${activePlayer.getPlayerName()} won!`));
+            return true;
+        }
+        return false;
     };
 
     return { getPositions, addPositions, getPlayerID, getPlayerName, checkWin};
@@ -21,33 +25,12 @@ function createPlayer(name, id){
 function createBoard(name, id, pos, activePlayer){
     const { getPositions, addPositions, getPlayerID, getPlayerName, checkWin} = createPlayer(name , id);
     let board=["-", "-", "-", "-", "-", "-", "-", "-", "-"];
-    winningCombo=[[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8]];
-    let curPlayer=0;
+    const getBoard = () => board;
+    const printBoard= () => console.log(board);
+    const markSquare = (pos, symbol) => { board[pos] = symbol; };
+    
 
-    const drop = (pos, activePlayer) =>{
-        let available=[0,1,2,3,4,5,6,7,8];
-        const combined=players[0].getPositions().concat(players[1].getPositions());
-        let remaining=available.filter(x => !combined.includes(x));
-
-        if(remaining.includes(pos)){
-            if(activePlayer.getPlayerID()==0){
-                board[pos]="X";
-
-            }
-            else{
-                board[pos]="O";
-                
-            }
-            activePlayer.addPositions(pos);
-            return true;
-        }
-        else{
-            return false;
-        }
-
-    }
-
-    return { getPositions, addPositions, getPlayerID, getPlayerName, checkWin, drop};
+    return { getBoard, printBoard, markSquare};
 
 }
 
@@ -70,11 +53,42 @@ function gameController(playerOneName="PlayerOne", playerTwoName="PlayerTwo"){
     }
     const getActivePlayer=() => activePlayer;
 
+
+    const drop = (pos, activePlayer) =>{
+        let available=[0,1,2,3,4,5,6,7,8];
+        const combined=players[0].getPositions().concat(players[1].getPositions());
+        let remaining=available.filter(x => !combined.includes(x));
+
+        if(remaining.includes(pos)){
+            if(activePlayer.getPlayerID()==0){
+                markSquare(pos, "X");
+
+            }
+            else{
+                markSquare(pos, "O");
+                
+            }
+            activePlayer.addPositions(pos);
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
     const playRound = (pos, activePlayer) => {
         if(drop(pos, activePlayer)==true){
             switchPlayer(activePlayer.getPlayerID());
-            
+            activePlayer.checkWin();
         }
     }
     return { switchPlayer, getActivePlayer, playRound};
 }
+
+const game = gameController();
+game.playRound(0);
+game.playRound(3);
+game.playRound(1);
+game.playRound(4);
+game.playRound(2); 
